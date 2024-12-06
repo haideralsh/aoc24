027a0accf102f156c079514b96f5efac0aff1461
Haider Alshamma <haidera@nulogy.com>
2024-12-05 22:15:03 -0500

    Refactor for conciseness

--- 2/part-1.js
+++ 2/part-1.js
@@ -1,27 +1,29 @@
 import { data } from "./data.js";
 
-const reports = data.split("\n").map((line) => line.split(/\s+/).map(Number));
-
 function isReportSafe(report) {
-	let firstDiff = report[1] - report[0]
-	let firstSign = Math.sign(firstDiff)
+  let firstDiff = report[1] - report[0];
+  let firstSign = Math.sign(firstDiff);
 
-	if (Math.abs(firstDiff) > 3 || Math.abs(firstDiff) < 1) return false
+  if (Math.abs(firstDiff) > 3 || Math.abs(firstDiff) < 1) return false;
 
-	for (let i = 1; i < report.length - 1; i++) {
-		let currentDiff = report[i + 1] - report[i]
+  for (let i = 1; i < report.length - 1; i++) {
+    let currentDiff = report[i + 1] - report[i];
 
-		if (
-			Math.sign(currentDiff) !== firstSign || 
-			Math.abs(currentDiff) > 3 || 
-			Math.abs(currentDiff) < 1				
-		) return false
-		
-	}
+    if (
+      Math.sign(currentDiff) !== firstSign ||
+      Math.abs(currentDiff) > 3 ||
+      Math.abs(currentDiff) < 1
+    )
+      return false;
+  }
 
-	return true
+  return true;
 }
 
-let safeReportCount = reports.map(isReportSafe).filter(Boolean).length
+let safeReportCount = data
+  .split("\n")
+  .map((line) => line.split(/\s+/).map(Number))
+  .map(isReportSafe)
+  .filter(Boolean).length;
 
-console.log({ safeReportCount })
+console.log({ safeReportCount });
