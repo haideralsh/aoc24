e102f4ea2403dfa177440c7ee99b770208a495bd
Haider Alshamma <haidera@nulogy.com>
2024-12-06 19:47:27 -0500

    Day 3 - part 1

---
+++ 3/part-1.js
@@ -0,0 +1,9 @@
+const raw = await Deno.readFile("./data.txt");
+const data = new TextDecoder().decode(raw);
+
+let res = [...data.matchAll(/mul\((\d+)\,(\d+)\)/g)]
+  .map(match => match.slice(1,3).map(Number))
+  .map(([x, y]) => x * y)
+  .reduce((sum, num) => sum + num, 0);
+
+console.log(res);
